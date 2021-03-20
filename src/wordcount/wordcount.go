bd231a3139a8dcf42d27f161f1b4865047ed6a67
linghduoduo <linghypshen@gmail.com>
2021-03-19 22:18:59 -0400

    add go code

--- src/wordcount/wordcount.go
+++ src/wordcount/wordcount.go
@@ -8,14 +8,12 @@ import (
 	"strings"
 )
 
-
 // CountWords counts the number of words in the specified
 // string and returns the count.
 func CountWords(text string) (count int) {
 	count = len(strings.Fields(text))
 	return
 }
-
 
 // main is the entry point for the application.
 func main() {
