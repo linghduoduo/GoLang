877b601c319141943e6bd50fd916e328a70c883e
linghduoduo <linghypshen@gmail.com>
2021-04-17 21:13:54 -0400

    add code

---
+++ src/CompositeTypes/compositetype.go
@@ -0,0 +1,22 @@
+package main
+
+// main is the entry point for the application.
+func main() {
+	//var x = [3]int
+	//fmt.Println(x)
+
+	////var x = [3]int{10, 20, 30}
+	//var x = [12]int{1, 5: 4, 6, 10: 100, 15}
+	//var x = [...]int{10, 20, 30}
+
+	//var x = [...]int{1, 2, 3}
+	//var y = [3]int{1, 2, 3}
+	//fmt.Println(x == y) // prints true
+	//fmt.Println(x)
+
+	//var x [2][3]int
+	//x[0] = [...]int{10, 20, 30}
+	//fmt.Println(x)
+
+	
+}
