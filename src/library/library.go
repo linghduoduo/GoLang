2713e2053a815f3c7f79a6bd5ad063e22593cafc
linghduoduo <linghypshen@gmail.com>
2021-05-31 00:02:30 -0400

    add code

---
+++ src/library/library.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+type Writer interface {
+	Write(p []byte) (n int, err error)
+}
+
+type NotHowReaderIsDefined interface {
+	Read() (p []byte, err error)
+}
+
+func countLetters(r io.Reader) (map[string]int, error) {
+	buf := make([]byte, 2048)
+	out := map[string]int{}
+	for {
+		n, err := r.Read(buf)
+		for _, b := range buf[:n] {
+			if (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z') {
+				out[string(b)]++
+			}
+		}
+		if err == io.EOF {
+			return out, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+}
+
+func main() {
+	s := "The quick brown fox jumped over the lazy dog"
+	sr := strings.NewReader(s)
+	counts, err := countLetters(sr)
+	if err != nil {
+		return
+	}
+	fmt.Println(counts)
+	
+}
