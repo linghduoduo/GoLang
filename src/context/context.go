847faf2c5d6187be15ba5941ae8ffb52a1779173
linghduoduo <linghypshen@gmail.com>
2021-06-02 09:38:29 -0400

    add code

--- src/context/context.go
+++ src/context/context.go
@@ -7,19 +7,20 @@ import (
 	"time"
 )
 
+//metadata that is required to correctly process the request, and metadata on when to stop processing the request.
 func logic(ctx context.Context, info string) (string, error) {
 	// do some interesting stuff here
 	return "", nil
 }
 
-func Middleware(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		ctx := req.Context()
-		// wrap the context with stuff -- we'll see how soon!
-		req = req.WithContext(ctx)
-		handler.ServeHTTP(rw, req)
-	})
-}
+//func Middleware(handler http.Handler) http.Handler {
+//	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+//		ctx := req.Context()
+//		// wrap the context with stuff -- we'll see how soon!
+//		req = req.WithContext(ctx)
+//		handler.ServeHTTP(rw, req)
+//	})
+//}
 
 func handler(rw http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
@@ -84,6 +85,86 @@ type ServiceCaller struct {
 //	}
 //}
 
+type userKey int
+const key userKey = 1
+
+func ContextWithUser(ctx context.Context, user string) context.Context {
+	return context.WithValue(ctx, key, user)
+}
+
+func UserFromContext(ctx context.Context) (string, bool) {
+	user, ok := ctx.Value(key).(string)
+	return user, ok
+}
+
+// a real implementation would be signed to make sure
+// the user didn't spoof their identity
+func extractUser(req *http.Request) (string, error) {
+	userCookie, err := req.Cookie("user")
+	if err != nil {
+		return "", err
+	}
+	return userCookie.Value, nil
+}
+
+func Middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		user, err := extractUser(req)
+		if err != nil {
+			rw.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		ctx := req.Context()
+		ctx = ContextWithUser(ctx, user)
+		req = req.WithContext(ctx)
+		h.ServeHTTP(rw, req)
+	})
+}
+
+//func (c Controller) handleRequest(rw http.ResponseWriter, req *http.Request) {
+//	ctx := req.Context()
+//	user, ok := identity.UserFromContext(ctx)
+//	if !ok {
+//		rw.WriteHeader(http.StatusInternalServerError)
+//		return
+//	}
+//	data := req.URL.Query().Get("data")
+//	result, err := c.Logic.businessLogic(ctx, user, data)
+//	if err != nil {
+//		rw.WriteHeader(http.StatusInternalServerError)
+//		rw.Write([]byte(err.Error()))
+//		return
+//	}
+//	rw.Write([]byte(result))
+//}
+
+type Logger interface {
+	Log(context.Context, string)
+}
+
+type RequestDecorator func(*http.Request) *http.Request
+
+type BusinessLogic struct {
+	RequestDecorator RequestDecorator
+	Logger                     Logger
+	Remote                     string
+}
+
+func (bl BusinessLogic) businessLogic(
+	ctx context.Context, user string, data string) (string, error) {
+	bl.Logger.Log(ctx, "starting businessLogic for " + user + " with "+ data)
+	req, err := http.NewRequestWithContext(ctx,
+		http.MethodGet, bl.Remote+"?query="+data, nil)
+	if err != nil {
+		bl.Logger.Log(ctx, "error building remote request:" + err)
+		return "", err
+	}
+	req = bl.RequestDecorator(req)
+	resp, err := http.DefaultClient.Do(req)
+	// processing continues
+	fmt.Println(resp)
+}
+
 func main() {
 	fmt.Println("Hello")
 
@@ -105,5 +186,9 @@ func main() {
 	end := time.Now()
 	fmt.Println(end.Sub(start))
 
-
+	bl := BusinessLogic{
+		RequestDecorator: tracker.Request,
+		Logger:           tracker.Logger{},
+		Remote:           "http://www.example.com/query",
+	}
 }
