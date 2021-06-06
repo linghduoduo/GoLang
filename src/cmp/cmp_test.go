b540724b620291420cd0feb7736d529fd3918578
linghduoduo <linghypshen@gmail.com>
2021-06-06 15:50:08 -0400

    add code

--- src/cmp/cmp_test.go
+++ src/cmp/cmp_test.go
@@ -2,9 +2,10 @@ package cmp
 
 import (
 "testing"
-
 "github.com/google/go-cmp/cmp"
 )
+
+//Use go-cmp to Compare Test Results
 
 func TestCreatePerson(t *testing.T) {
 	expected := Person{
@@ -12,7 +13,14 @@ func TestCreatePerson(t *testing.T) {
 		Age:  37,
 	}
 	result := CreatePerson("Dennis", 37)
-	if diff := cmp.Diff(expected, result); diff != "" {
+	//if diff := cmp.Diff(expected, result); diff != "" {
+	//	t.Error(diff)
+	//}
+
+	comparer := cmp.Comparer(func(x, y Person) bool {
+		return x.Name == y.Name && x.Age == y.Age
+	})
+	if diff := cmp.Diff(expected, result, comparer); diff != "" {
 		t.Error(diff)
 	}
 }
